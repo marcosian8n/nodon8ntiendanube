efae5b0aef3166dbfde6d0283565952882c1ff28
marcosian8n <marcos.airaldi@gmail.com>
2025-05-28 14:31:13 -0300

    Create TiendanubeOAuth2Api.credentials.ts

---
+++ credentials/TiendanubeOAuth2Api.credentials.ts
@@ -0,0 +1,31 @@
+import {
+  ICredentialType,
+  INodeProperties,
+} from 'n8n-workflow';
+
+export class TiendanubeOAuth2Api implements ICredentialType {
+  name = 'tiendanubeOAuth2Api';
+  displayName = 'TiendaNube OAuth2 API';
+  documentationUrl = 'https://dev.tiendanube.com/docs/api';
+  extends = ['oAuth2Api'];
+  properties: INodeProperties[] = [
+    {
+      displayName: 'Client ID',
+      name: 'clientId',
+      type: 'string',
+      default: '',
+    },
+    {
+      displayName: 'Client Secret',
+      name: 'clientSecret',
+      type: 'string',
+      default: '',
+    },
+    {
+      displayName: 'Redirect URI',
+      name: 'redirectUri',
+      type: 'string',
+      default: '',
+    },
+  ];
+}
