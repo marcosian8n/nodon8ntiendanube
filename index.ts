5666dea86c77625960675b8ca33ca7e6175d676a
marcosian8n <marcos.airaldi@gmail.com>
2025-05-28 14:30:42 -0300

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,5 @@
+import { Tiendanube } from './nodes/Tiendanube/Tiendanube.node';
+import { TiendanubeOAuth2Api } from './credentials/TiendanubeOAuth2Api.credentials';
+
+export const nodes = [Tiendanube];
+export const credentials = [TiendanubeOAuth2Api];
