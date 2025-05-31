16db66238d055d7df81c252e5f7b5515ea30dca1
marcosian8n <marcos.airaldi@gmail.com>
2025-05-30 23:57:48 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { Tiendanube } from './nodes/Tiendanube/Tiendanube.node';
-import { TiendanubeOAuth2Api } from './credentials/TiendanubeOAuth2Api.credentials';
+import { TiendanubeApi } from './credentials/TiendanubeApi.credentials';
 
 export const nodes = [Tiendanube];
 export const credentials = [TiendanubeOAuth2Api];
